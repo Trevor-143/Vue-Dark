42c5332572987bb5d7c90463c5d981aeb0d18f9e
Arapu Trevor <araputrevor2@gmail.com>
2023-01-12 13:04:44 +0300

    vue.js dark mode

---
+++ src/components/Content.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="content">
+        <h3>This is {{ mode }} mode</h3>
+        <div class="image-grid">
+            <div class="col">
+                <img src="https://cdn.pixabay.com/photo/2016/02/28/12/55/boy-1226964__340.jpg" alt="">
+            </div>
+            <div class="col">
+                <img src="https://cdn.pixabay.com/photo/2012/12/27/19/40/chain-link-72864__340.jpg" alt="">
+            </div>
+            <div class="col">
+                <img src="https://cdn.pixabay.com/photo/2017/09/25/11/55/cyberspace-2784907__340.jpg" alt="">
+            </div>
+        </div>
+        <p class="para">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quae cum natus quia odio, accusantium adipisci, suscipit quibusdam cupiditate nostrum, neque voluptatum ea id. Laudantium consectetur repudiandae velit officiis. Amet esse iure omnis nesciunt corporis. Exercitationem voluptate enim numquam quaerat ducimus at dignissimos quasi, quo sunt nemo nisi similique est?
+        </p>
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: ['mode']
+}
+</script>
+
+<style>
+.content {
+    padding: 25px;
+}
+.content h3 {
+    font-size: 28px;
+    text-transform: uppercase;
+    font-weight: 300;
+    margin-bottom: 25px;
+}
+.image-grid {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 15px;
+}
+.image-grid .col img {
+    width: 100%;
+}
+.para {
+    width: 100%;
+    max-width: 600px;
+    margin: 25px auto 0px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,60 @@
+<template>
+    <header>
+        <h1>{{ mode }} Mode</h1>
+        <Toggle :mode="mode" @toggle="$emit('toggle')" />
+        <nav>
+            <div class="nav-links">Home</div>
+            <div class="nav-links">About</div>
+            <div class="nav-links">Portfolio</div>
+            <div class="nav-links">Contact</div>
+        </nav>
+    </header>
+
+</template>
+
+<script>
+import Toggle from "@/components/Toggle";
+
+export default {
+    props: [ "mode" ],
+    components: {
+        Toggle
+    }
+}
+</script>
+
+<style>
+header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0 25px;
+    height: 60px;
+    background: #d3d3d3;
+    transition: background 0.3s ease-in-out;
+}
+.dark header {
+    background: #15202b;
+}
+header h1 {
+    font-size: 32px;
+    text-transform: uppercase;
+    font-weight: 900;
+}
+header nav {
+    display: flex;
+    height: 60px;
+}
+header nav .nav-links {
+    display: flex;
+    align-items: center;
+    padding: 0 15px;
+}
+header nav .nav-links:hover {
+    background: #b0b0b0;
+}
+.dark header nav .nav-links:hover {
+    background: #101520;
+}
+
+</style>
